87f0044e3e04bbafeb577ee48b0b8dfde1085d73
Terence Martin <odatnurd@gmail.com>
2025-12-03 22:00:27 -0600

    Allow for generic combinators on command checks
    The previous implementation of command checks assumed that all checks
    get the same arguments no matter what (a plugin). For editor commands,
    we actually get the editor and the view to work within as well.
    
    This is fine except that for our combinator functions and(), or() and
    not(), those assume always a single argument.
    
    This command makes those functions take arbitrary arguments and will
    just pass them along.

--- src/utils/command_combiners.ts
+++ src/utils/command_combiners.ts
@@ -1,8 +1,13 @@
 /******************************************************************************/
 
 
-import { Plugin } from 'obsidian';
-import { type CheckFunction } from '#utils/command_factory';
+/* A generic type for arguments; this allows the function to adapt to whatever
+ * arguments it happens to be provided, allowing it to be used by any command
+ * type.
+ *
+ * This also allows sharing of check functions between different command types,
+ * so long as they are careful about their arguments. */
+type GenericCheckFunction<Args extends unknown[]> = (...args: Args) => boolean;
 
 
 /******************************************************************************/
@@ -10,8 +15,8 @@ import { type CheckFunction } from '#utils/command_factory';
 
 /* This factory combiner will return a check function that returns true only
  * when every check function it is given returns true. */
-export function and(...checks: CheckFunction[]) : CheckFunction {
-  return (plugin: Plugin) => checks.every(check => check(plugin)) === true;
+export function and<Args extends unknown[]>(...checks: GenericCheckFunction<Args>[]) : GenericCheckFunction<Args> {
+  return (...args: Args) => checks.every(check => check(...args)) === true;
 }
 
 
@@ -20,8 +25,8 @@ export function and(...checks: CheckFunction[]) : CheckFunction {
 
 /* This factory combiner will return a check function that returns true if any
  * of the provided check functions it is given returns true. */
-export function or(...checks: CheckFunction[]) : CheckFunction {
-  return (plugin: Plugin) => checks.some(check => check(plugin)) === true;
+export function or<Args extends unknown[]>(...checks: GenericCheckFunction<Args>[]) : GenericCheckFunction<Args> {
+  return (...args: Args) => checks.some(check => check(...args)) === true;
 }
 
 
@@ -31,8 +36,8 @@ export function or(...checks: CheckFunction[]) : CheckFunction {
 /* This factory combiner takes only a single check function and returns true
  * only when the check function itself returns false, thereby inverting the
  * state of that check. */
-export function not(check: CheckFunction) : CheckFunction {
-  return (plugin: Plugin) => check(plugin) === false;
+export function not<Args extends unknown[]>(check: GenericCheckFunction<Args>) : GenericCheckFunction<Args> {
+  return (...args: Args) => check(...args) === false;
 }
 
 
